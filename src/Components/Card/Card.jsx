d0ef9ec1d4dc97a8a5cae843b3e107c9d40d0ecf
mdridoyali <mdridoyislamr@gmail.com>
2023-09-25 10:00:56 +0600

    donate cards are shown in the donate page

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,23 +1,50 @@
 import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
+import swal from 'sweetalert';
 
 const Card = () => {
   const [showCard, setShowCard] = useState([]);
 
   const { id } = useParams();
   const cards = useLoaderData();
+  console.log(showCard)
+
+  const handleAddToDonate=()=>{
+    const addToDonate = [];
+    const donateItems = JSON.parse(localStorage.getItem('donation'));
+    if(!donateItems){
+      addToDonate.push(showCard);
+      localStorage.setItem('donation', JSON.stringify(addToDonate) )
+      swal("Good job!", "You added donation!", "success");
+    }else{
+      const isExits = donateItems.find((showCard) => showCard.id === id);
+      if(!isExits){
+        addToDonate.push(...donateItems, showCard);
+        localStorage.setItem('donation', JSON.stringify(addToDonate));
+        swal("Good job!", "You added donation!", "success");
+      }
+      else{
+        swal("Don't Duplicate!", "Already added this item!", "error");
+      }
+
+    }
+
+    // const 
+    console.log('ok')
+  }
 
   useEffect(() => {
     const findCard = cards.find((card) => card.id === id);
     setShowCard(findCard);
-    console.log(findCard);
+
   }, [id, cards]);
 
   return (
     <div className="flex justify-center items-center h-[60vh]  ">
-      <div className="w-11/12 mx-auto flex flex-col justify-center items-center  " >
-        <img className="w-[30vw] mt-10 " src={showCard.image}  />
-        <button style={{backgroundColor: showCard.text_color}} className="px-2 py-1 text-lg font-semibold text-white rounded-md "> Donate ${showCard.price}</button>
+      <div className="w-11/12 mx-auto flex flex-col justify-center items-center space-y-2 " >
+        <img className="w-[30vw] mt-10 rounded-xl " src={showCard.image}  />
+        <button onClick={handleAddToDonate} style={{backgroundColor: showCard.text_color}} className="px-2 py-1 text-lg font-semibold text-white rounded-md "> Donate ${showCard.price}</button>
+        <h2 className="text-xl font-bold">{showCard.title}</h2>
         <p className="">{showCard.description}</p>
       </div>
     </div>
--- src/Components/Donation/Donation.jsx
+++ src/Components/Donation/Donation.jsx
@@ -1,12 +1,62 @@
-
+import { useEffect, useState } from "react";
 
 const Donation = () => {
-    return (
-        <div>
-            Donation
-            
-        </div>
-    );
+  const [donates, setDonates] = useState([]);
+  const [seeAll, setSeeAll] = useState(4);
+
+
+  console.log(donates);
+
+  useEffect(() => {
+    const donateItems = JSON.parse(localStorage.getItem("donation"));
+    if (donateItems) {
+      setDonates(donateItems);
+    }
+  }, []);
+
+  const seeAllBtn = ()=>{
+   setSeeAll(donates.length)
+  }
+
+  return (
+    <div className="mb-10">
+      <div className="w-11/12 mt-10 mb-10 mx-auto grid grid-cols-1 md:grid-cols-2 gap-7 justify-between">
+        {donates.slice(0, seeAll).map((donate, idx) => (
+          <div
+            key={idx}
+            style={{ backgroundColor: donate.card_bg }}
+            className="rounded-lg flex "
+          >
+            <img className="" src={donate.image_don} alt={donate.title} />
+            <div className="p-4 space-y-4">
+              <p
+                style={{
+                  backgroundColor: donate.category_bg,
+                  color: donate.text_color,
+                }}
+                className="w-min px-3 rounded font-semibold"
+              >
+                {donate.category}
+              </p>
+              <h2 className="font-bold text-xl">{donate.title}</h2>
+              <p style={{ color: donate.text_color }} className="font-bold ">
+                ${donate.price.toFixed(2)}{" "}
+              </p>
+              <button
+                style={{ backgroundColor: donate.text_color }}
+                className="px-2 py-1 rounded-md text-white  "
+              >
+                View Details
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="mx-auto text-center ">
+        <button onClick={seeAllBtn} className="bg-green-500 hover:bg-green-600 px-4  text-lg font-semibold text-white rounded-md py-1 ">See All</button>
+      </div>
+    </div>
+  );
 };
 
 export default Donation;
